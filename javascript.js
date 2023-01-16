ec7e86235a842fd159ead4f88cac3c5e368a53a4
Aleksandr <sancho2140@gmail.com>
2023-01-16 20:48:41 +0300

    update 3

--- javascript.js
+++ javascript.js
@@ -19,3 +19,75 @@ navToggle.addEventListener('click', () => {
     }
     
 })
+
+// ! Carousel animation
+
+
+const container = document.querySelector('.moving-container')
+const slides = Array.from(container.children)
+const prevButton = document.querySelector('.left')
+const nextButton = document.querySelector('.right')
+
+const dotsNav = document.querySelector('.dots')
+const dots = Array.from(dotsNav.children)
+
+// const slideWidth = slides[0].getBoundingClientRect().width
+
+const setSlidePosition = (slide, index) => {
+    slide.style.left = 100 * index + '%'
+}
+slides.forEach(setSlidePosition)
+
+// *Reusable funnctions
+
+const updateDots = (currentDot, nextDot) => {
+    currentDot.classList.remove('current-dot')
+    nextDot.classList.add('current-dot')
+}
+
+
+const updateSlide = (container, currentSlide, nextSlide) => {
+    container.style.transform = 'translateX(-' + nextSlide.style.left + ')'
+    currentSlide.classList.remove('current')
+    nextSlide.classList.add('current')
+}
+
+// *The end
+
+
+nextButton.addEventListener('click', () => {
+    const currentSlide = document.querySelector('.current')
+    const nextSlide = currentSlide.nextElementSibling
+
+    const currentDot = dotsNav.querySelector('.current-dot')
+    const nextDot = currentDot.nextElementSibling
+
+    updateSlide(container, currentSlide, nextSlide)
+    updateDots(currentDot, nextDot)
+
+})
+
+prevButton.addEventListener('click', () => {
+    const currentSlide = document.querySelector('.current')
+    const prevSlide = currentSlide.previousElementSibling
+
+    const currentDot = dotsNav.querySelector('.current-dot')
+    const prevtDot = currentDot.previousElementSibling
+
+    updateSlide(container, currentSlide, prevSlide)
+    updateDots(currentDot, prevtDot)
+})
+
+dotsNav.addEventListener('click', e=> {
+    
+    const targetDot = e.target.closest('div')
+    if (!targetDot) return
+
+    const currentSlide = container.querySelector('.current')
+    const currentDot = dotsNav.querySelector('.current-dot')
+    const targetIndex = dots.findIndex( dot => dot === targetDot )
+    const targetSlide = slides[targetIndex]
+
+    updateSlide(container, currentSlide, targetSlide)
+    updateDots(currentDot, targetDot)
+})
